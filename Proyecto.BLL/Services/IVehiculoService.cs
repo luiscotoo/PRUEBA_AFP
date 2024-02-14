ca5b06c93401ffcb9a02c039eb0b523ae8e0e15b
luiscotoo <luiscoto001@gmail.com>
2024-02-14 11:24:20 -0600

    BLL: Creando interfaz de services y clase que lo implementa

---
+++ Proyecto.BLL/Services/IVehiculoService.cs
@@ -0,0 +1,18 @@
+﻿using Proyecto.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto.BLL.Services
+{
+    public interface IVehiculoService
+    {
+        Task<bool> Insertar(Vehiculo modelo);
+        Task<bool> Actualizar(Vehiculo modelo);
+        Task<bool> Eliminar(int id);
+        Task<Vehiculo> Obtener(int id);
+        Task<IQueryable<Vehiculo>> ObtenerTodos();
+    }
+}
---
+++ Proyecto.BLL/Services/VehiculoService.cs
@@ -0,0 +1,45 @@
+﻿using Proyecto.DAL.Repositories;
+using Proyecto.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto.BLL.Services
+{
+    public class VehiculoService : IVehiculoService
+    {
+        private readonly IGenericRepository<Vehiculo> _repository;
+        
+        public VehiculoService(IGenericRepository<Vehiculo> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> Actualizar(Vehiculo modelo)
+        {
+            return await _repository.Actualizar(modelo);
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            return await _repository.Eliminar(id);
+        }
+
+        public async Task<bool> Insertar(Vehiculo modelo)
+        {
+            return await _repository.Insertar(modelo);
+        }
+
+        public async Task<Vehiculo> Obtener(int id)
+        {
+            return await _repository.Obtener(id);
+        }
+
+        public async Task<IQueryable<Vehiculo>> ObtenerTodos()
+        {
+            return await _repository.ObtenerTodos();
+        }
+    }
+}
