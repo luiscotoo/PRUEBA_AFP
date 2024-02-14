ceb17796667913a8c37711f6c2856691f3602030
luiscotoo <luiscoto001@gmail.com>
2024-02-14 11:02:10 -0600

    Creando modelos y versionando script de bd

---
+++ Proyecto.Models/Models/Vehiculo.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proyecto.Models.Models
+{
+    [Table("VEHICULO")]
+    public class Vehiculo
+    {
+        [Key]
+        [Column("ID_VEHICULO")]
+        public int IdVehiculo { get; set; }
+
+        [Column("NUMERO_PLACA")]
+        [StringLength(20)]
+        public string? NumeroPlaca { get; set; }
+
+        [Column("VIN")]
+        [StringLength(17)]
+        public string? Vin { get; set; }
+
+        [Column("MARCA")]
+        [StringLength(50)]
+        public string? Marca { get; set; }
+
+        [Column("SERIE")]
+        [StringLength(50)]
+        public string? Serie { get; set; }
+
+        [Column("Anio")]
+        public int? Anio { get; set; }
+
+        [Column("COLOR")]
+        [StringLength(50)]
+        public string? Color { get; set; }
+
+        [Column("CANTIDAD_PUERTAS")]
+        public int? CantidadPuertas { get; set; }
+
+    }
+}
